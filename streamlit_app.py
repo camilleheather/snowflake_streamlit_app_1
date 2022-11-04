2cc9a9315102e12eeb7e3dbaa92531f3f8a05862
Camille <117263352+camilleheather@users.noreply.github.com>
2022-11-04 11:54:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,17 +51,30 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 
+
+
+
+streamlit.header("My Fruit Load list contains:")
+
+#Snowflake functions
+def get_fruit_load_list():
+    with  my_cnx.cursor() as my_cur:
+          my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+          return my_cur.fetchall()
+
+        
+#Add button to load fruit        
+
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+    
+    
 #troubleshooting
 streamlit.stop()
 
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.header("My Fruit Load list contains:")
-streamlit.dataframe(my_data_rows)
 
 # Allow end user to add a fruit to the list
 
@@ -69,6 +82,11 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfru
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute("INSERT INTO FRUIT_LOAD_LIST values ('from streamlit')")
+
+
+
+
+
 
 
 
